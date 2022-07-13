796f71b6fe460ed1320feba9d199fbb3a95a9163
EderBrunoP <ederbruno15@gmail.com>
2022-07-12 23:18:56 -0500

    ejemplo

--- semana_6/dia_2_pi_trainer/index.js
+++ semana_6/dia_2_pi_trainer/index.js
@@ -2,21 +2,11 @@
 const input_username = document.querySelector("#username");
 const button_start = document.querySelector("#start");
 const div_game = document.querySelector("#game");
+let user = null;
 
 input_username.addEventListener("keyup", function () {
   button_start.disabled = this.value == "";
 });
-
-//* JSON.parse: string => objeto
-//* JSON.stringify: objeto => string
-
-//* Para poder recuperar un dato de localStorage podemos usar las funciion
-//* localStorage.getItem("key")
-//* El parametro quen recibe getItem es el key
-
-console.log("username", localStorage.getItem("user_name"));
-// Para convertir de string a objeto
-console.log("objeto user", JSON.parse(localStorage.getItem("obj_user")));
 
 button_start.addEventListener("click", function () {
   this.parentElement.querySelector(
@@ -24,25 +14,11 @@ button_start.addEventListener("click", function () {
   ).innerText += `Hello ${input_username.value}, let's play`;
   this.parentElement.querySelector("h2").style.display = "block";
 
-  // vamos a guardar el nombre del usuario el localStorage
-  // para usar esto hacemos lo siguiente
-
-  //* localStorage.setItem = Es la funcion que pormite guardar un string en localStorage
-  //* este recibe 2 parametros
-  //* 1 => key
-  //* 2 => value
-  localStorage.setItem("user_name", input_username.value);
-  // para guardar datos mas complejos
-  const objetoUser = {
-    user_name: input_username.value,
-    created_at: new Date(),
-  };
-  //JSON.stringify(): Convierte objetos a strings
-  localStorage.setItem("obj_user", JSON.stringify(objetoUser));
+  // creamos el user:
+  user = new User(input_username.value);
 
   input_username.disabled = true;
   this.disabled = true;
-
   div_game.style.display = "flex";
 });
 
@@ -94,7 +70,7 @@ input_pi.addEventListener("keydown", function (evt) {
   ) {
     div_game.style.backgroundColor = "red";
     failed_attempts++;
-    showModal(failed_attempts);
+    evalGameOver();
   } else {
     // si pasó las reglas:
     // recuerden que la primera vez position es 0
@@ -122,43 +98,80 @@ input_pi.addEventListener("keydown", function (evt) {
 });
 
 function calcScore(now_attempt) {
-  // tenemos cunto timepo en milisegundo nos demoramos
-  // en escribir cada numero
-
-  //   if (position > 10) {
-  //     const digito = String(position).charAt(0);
-  //     score += 10 + Number(digito) - diff_time * 0.01;
-  //   } else {
-  //     score += 10 - diff_time * 0.01;
-  //   }
   const diff_time = (now_attempt - last_attempt) * 0.01;
-
   const attempt_score =
     position > 10
       ? 10 + Number(String(position).charAt(0)) - diff_time
       : 10 - diff_time;
-
   score += Math.max(attempt_score, -1);
-  // el puntaje sera
-  // los primeros 0 - 10 aciertos seran
-  // score = 10 + 0 - (diff_time * 0.01)
-  // cuando lleguemos a 11 - 19
-  // score = 10 + 1 - (diff_time * 0.01)
-  // cuando lleguemos a 20 - 29
-  // score = 10 + 2 - (diff_time * 0.01)
-  // cuando lleguemos a 30 - 39
-  // score = 10 + 3 - (diff_time * 0.01)
 }
 
-function showModal(failed_attempts) {
-  if (failed_attempts === 10) {
+function evalGameOver() {
+  if (failed_attempts >= 10) {
     // activamos los estilos
-    container_modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-    lost_container.style.display = "block";
-    container.style.zIndex = -1;
-    input_pi.disabled = true;
-    h3_score_finish.querySelector("span").innerText = score.toFixed(2);
+    setStyles();
+
+    if (user === null) return;
+
+    // caso juanito
+    const user_index = findIndexUser();
+    // juanito no existe en el array de usuarios por ende la funcion findIndex retorna
+    // -1, porque no encontro a la persona
+    // nota: Si findIndex no encuentra lo busquedo retornara -1 porque no encontro
+    // ni una posicion
+    if (user_index !== -1) {
+      users[user_index].games.push(setObjectGame());
+      updateUserLocalStorage(users);
+      return;
+    }
+
+    user.games.push(setObjectGame());
+    addUserToLocalStorage(user);
   }
 }
 
+function setStyles() {
+  // disabled scroll
+  document.body.style.overflow = "hidden";
+  container_modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  container_modal.style.position = "relative";
+  container_modal.style.height = "100vh";
+  container.style.position = "absolute";
+  lost_container.style.display = "block";
+  container.style.zIndex = -1;
+  input_pi.disabled = true;
+  h3_score_finish.querySelector("span").innerText = score.toFixed(2);
+}
+
+function findIndexUser() {
+  return users.findIndex((user_find) => user_find.username === user.username);
+}
+
+function setObjectGame() {
+  return {
+    score,
+    attempts,
+    success_attempts,
+    failed_attempts,
+    gameover_at: user.gameover(),
+  };
+}
+
 btn_reiniciar.onclick = () => window.location.reload();
+
+// buscador
+const btnBuscar = document.querySelector("#btn-buscar");
+const inputBuscar = document.querySelector("#input-buscar");
+
+btnBuscar.onclick = function () {
+  // queremos buscar en el array de users
+  const username = inputBuscar.value;
+
+  const filterUsers = users.filter((user) =>
+    user.username.toLowerCase().includes(username)
+  );
+
+  createTableHistoric(filterUsers);
+
+  inputBuscar.value = "";
+};
